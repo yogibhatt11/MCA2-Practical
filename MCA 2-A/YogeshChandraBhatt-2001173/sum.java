be23cb0a9764b880b0e970c7d42939fc37c4ad8d
Yogesh Chandra Bhatt <56780337+yogibhatt11@users.noreply.github.com>
2021-06-18 13:44:02 +0530

    Add files via upload

---
+++ MCA 2-A/YogeshChandraBhatt-2001173/sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class sum{
+	public static void main(String args[]){
+		int num1,num2,num3,result;
+		System.out.println("Enter any 3 integers ");
+		Scanner in = new Scanner(System.in);
+		num1 = in.nextInt();
+		num2 = in.nextInt();
+		num3 = in.nextInt();
+		result=num1+num2+num3;
+		System.out.println("The result after addition is "+result);
+	}
+}
